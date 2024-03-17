75667cf2e94598112a31a6d21921e009e33e0ac4
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2024-03-17 07:26:17 -0400

    Ugly Number

---
+++ problems/0263/0263.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isUgly = function (n) {
+  if (n === 1) return true;
+  if (n <= 0) return false;
+
+  while (n !== 1) {
+    if (n % 2 === 0) n /= 2;
+    else if (n % 3 === 0) n /= 3;
+    else if (n % 5 === 0) n /= 5;
+    else return false;
+  }
+
+  return true;
+};
