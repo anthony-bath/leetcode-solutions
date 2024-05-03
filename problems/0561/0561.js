01b79eb7fb42c950bf6efaad297216e9186f8cbc
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2024-05-03 07:29:47 -0400

    Array Partition

---
+++ problems/0561/0561.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i += 2) {
+    sum += nums[i];
+  }
+
+  return sum;
+};
