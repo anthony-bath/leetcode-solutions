52b976a6803e72a085f4adb4c1e2bf6dc3f50345
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2024-09-15 14:34:46 -0400

    Can Make Arithmetic Progression From Sequence

---
+++ problems/1502/1502.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const canMakeArithmeticProgression = function (arr) {
+  if (arr.length === 2) return true;
+
+  arr.sort((a, b) => a - b);
+
+  const diff = arr[1] - arr[0];
+
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== diff) return false;
+  }
+
+  return true;
+};
