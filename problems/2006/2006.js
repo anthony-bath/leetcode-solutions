75d4695c4ee4356b1c90c671b2f84c7b8a3d9cf3
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2025-01-06 12:49:06 -0500

    Count Number of Pairs With Absolute Difference K

---
+++ problems/2006/2006.js
@@ -0,0 +1,20 @@
+const { abs } = Math;
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const countKDifference = function (nums, k) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (abs(nums[i] - nums[j]) === k) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
