48766a00908af09f27deecb62e7489b3e3737cde
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2025-04-03 12:23:03 -0400

    Range Sum Query - Immutable

---
+++ problems/0303/0303.js
@@ -0,0 +1,22 @@
+class NumArray {
+  nums;
+  prefixSum;
+
+  constructor(nums) {
+    this.nums = nums;
+    this.prefixSum = Array(nums.length).fill(0);
+
+    for (let i = 0; i < nums.length; i++) {
+      this.prefixSum[i + 1] = this.prefixSum[i] + nums[i];
+    }
+  }
+
+  sumRange(left, right) {
+    return this.prefixSum[right + 1] - this.prefixSum[left];
+  }
+}
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
