d0db2173bf5ed01a5141f7f15986d3a31710065f
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2025-06-04 21:23:45 -0400

    Find Peak Element

---
+++ problems/0162/0162.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findPeakElement = function (nums) {
+  let [left, right] = [0, nums.length - 1];
+
+  while (left < right) {
+    const mid = (left + right) >> 1;
+
+    if (nums[mid] > nums[mid + 1]) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+};
