3e57aa0e439a84e0d830bd81a29207d2aafa4e0a
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2026-01-25 08:45:27 -0500

    Uncrossed Lines

---
+++ problems/1035/1035.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const maxUncrossedLines = function (nums1, nums2) {
+  const m = nums1.length,
+    n = nums2.length;
+  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (nums1[i - 1] === nums2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[m][n];
+};
