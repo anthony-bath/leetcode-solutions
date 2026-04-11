c9f8ef105ad51c94e91d0c796421ddb6e1ba69bb
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2026-04-11 19:33:35 -0400

    Debounce

---
+++ problems/2501-3000/2627/2627.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+const debounce = function (fn, t) {
+  let timer;
+
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(fn, t, ...args);
+  };
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
